a80f682182e5d66b52a57ee4fa12800f393a0789
Amandeep Kumar <hiaman.dev@gmail.com>
2022-03-22 22:52:29 +0530

    Added method to delete contacts and retain records after page refreshes

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,16 +1,14 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
 
+    const removeContactHandler = (id) => {
+        props.getContactId(id)
+    };
     const renderContactList = props.contacts.map((contact) => {
         return (
-            <div className="item">
-                <div className="content">
-                    <div className="header">{contact.name}</div>
-                    <div>{contact.email}</div>
-                </div>
-                <i className="trash alternate outline icons"></i>
-            </div>
+            <ContactCard contact= {contact} clickHandler={removeContactHandler} key={contact.id} />
         );
     });
 
